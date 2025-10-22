25f96c6780b7dac146ac94269b0c2b604d8f771f
Luke-Dimond <LukeManuelMG@gmail.com>
2025-10-22 12:36:29 -0400

    added an array class that can be created with lsl::array<type> arr(size), will fix later

---
+++ lsl/array.hpp
@@ -0,0 +1,67 @@
+/*
+* Created by: Luke Manuel
+* Description: A custom array made to mimic std::array
+* 2025
+*/
+
+#pragma once
+#include <cstddef>
+#include <stdexcept>
+
+namespace lsl{
+    template <typename T>
+    class array{
+
+    public:
+
+        // Constructor and Destructor
+        array(size_t size)
+            : data_(new T[size]{}), size_(size)
+        {
+        }
+
+        ~array() {
+                delete[] data_;
+            }
+
+
+        // Functions
+        size_t length() const {
+            return size_;
+        }
+
+        const T* begin() const {
+            return data_;
+        }
+
+        const T* end() const {
+            return data_ + size_;
+        }
+
+
+
+        // Operators
+        array& operator= (const array& arr){
+
+            if (size_ != arr.length()){
+                throw std::invalid_argument("Both arrays must be of equal length");
+            }
+
+            for(size_t i = 0; i < size_; i++){
+                data_[i] = arr[i];
+            }
+
+            return *this;
+
+        }
+
+        T& operator[] (size_t i) const {
+            return data_[i];
+        }
+
+    private:
+
+        T* data_{nullptr};
+        size_t size_{};
+    };
+}
--- lsl/lsl.hpp
+++ lsl/lsl.hpp
@@ -5,3 +5,4 @@
 
 #pragma once
 #include "vector.hpp"
+#include "array.hpp"
