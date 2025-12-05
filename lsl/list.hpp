f646de77d7fe20ec8dedb933934d77c618feae30
Luke-Dimond <LukeManuelMG@gmail.com>
2025-12-05 14:50:41 -0500

    added a double linked list hpp

---
+++ lsl/list.hpp
@@ -0,0 +1,116 @@
+#include <stdexcept>
+#include <cstddef>
+
+template <typename T>
+struct node {
+    T value{};
+    node<T>* nextNode{};
+    node<T>* prevNode{};
+};
+
+template <typename T>
+class List {
+
+private: 
+    node<T>* head{};
+    node<T>* tail{};
+    size_t size{};
+
+public: 
+
+    List() = default;
+
+    ~List() {
+        clear();
+    }
+
+    List(const List& newList) {
+        for (node<T>* cur = newList.head; cur != nullptr; cur = cur->nextNode) {
+            push_back(cur->value);
+        }
+    }
+
+    //Clear the list and free all nodes 
+    void clear() {
+        node<T>* curNode = head;
+
+        while(curNode != nullptr) {
+
+            node<T>* nextNode = curNode->nextNode;
+            delete curNode;
+            curNode = nextNode;
+
+        }
+
+        head = tail = nullptr;
+        size = 0;
+    }
+
+    void push_front (const T& v) {
+        node<T>* newNode = new node<T>{v, head, nullptr};
+
+        if (head != nullptr) head->prevNode = newNode;
+
+        head = newNode;
+        if (tail == nullptr) tail = newNode;
+        size++;
+    }
+
+    void push_back (const T& v) {
+        node<T>* newNode = new node<T>{v, nullptr, tail};
+        
+        if (tail != nullptr) {
+            tail->nextNode = newNode;
+        } else {
+            head = newNode;
+        }
+        
+        tail = newNode;
+
+        size++;
+    } 
+
+    void pop_front() {
+        if (head == nullptr) throw std::runtime_error("List is empty");
+
+        node<T>* temp = head;
+        head = head->nextNode;
+
+        if (head != nullptr) {
+            head->prevNode = nullptr;
+        } else {
+            tail = nullptr;
+        }
+
+        delete temp;
+        size--;
+    }
+
+    void pop_back() {
+        if (tail == nullptr) throw std::runtime_error("List is empty");
+
+        node<T>* temp = tail;
+        tail = tail->prevNode;
+
+        if (tail != nullptr) {
+            tail->nextNode = nullptr;
+        } else {
+            head = nullptr;
+        }
+
+        delete temp;
+        size--;
+    }
+
+    List& operator=(const List& rhs) {
+        if (this == &rhs) return *this;
+        clear();
+
+        for (node<T>* cur = rhs.head; cur != nullptr; cur = cur->nextNode) {
+            push_back(cur->value);
+        }
+
+        return *this;
+    }
+
+};
