bee6709d1e81dd6cb0e39bf9d923a87be4afe9d1
Luke-Dimond <LukeManuelMG@gmail.com>
2025-12-05 14:55:49 -0500

    put list.hpp in namespace lsl

--- lsl/list.hpp
+++ lsl/list.hpp
@@ -1,116 +1,118 @@
 #include <stdexcept>
 #include <cstddef>
 
-template <typename T>
-class List {
+namespace lsl {
+    template <typename T>
+    class List {
 
-private: 
+    private: 
 
-    struct node {
-        T value{};
-        node<T>* nextNode{};
-        node<T>* prevNode{};
-    };
+        struct node {
+            T value{};
+            node<T>* nextNode{};
+            node<T>* prevNode{};
+        };
 
-    node<T>* head{};
-    node<T>* tail{};
-    size_t size{};
+        node<T>* head{};
+        node<T>* tail{};
+        size_t size{};
 
-public: 
+    public: 
 
-    List() = default;
+        List() = default;
 
-    ~List() {
-        clear();
-    }
-
-    List(const List& newList) {
-        for (node<T>* cur = newList.head; cur != nullptr; cur = cur->nextNode) {
-            push_back(cur->value);
-        }
-    }
-
-    //Clear the list and free all nodes 
-    void clear() {
-        node<T>* curNode = head;
-
-        while(curNode != nullptr) {
-
-            node<T>* nextNode = curNode->nextNode;
-            delete curNode;
-            curNode = nextNode;
-
+        ~List() {
+            clear();
         }
 
-        head = tail = nullptr;
-        size = 0;
-    }
+        List(const List& newList) {
+            for (node<T>* cur = newList.head; cur != nullptr; cur = cur->nextNode) {
+                push_back(cur->value);
+            }
+        }
 
-    void push_front (const T& v) {
-        node<T>* newNode = new node<T>{v, head, nullptr};
+        //Clear the list and free all nodes 
+        void clear() {
+            node<T>* curNode = head;
 
-        if (head != nullptr) head->prevNode = newNode;
+            while(curNode != nullptr) {
 
-        head = newNode;
-        if (tail == nullptr) tail = newNode;
-        size++;
-    }
+                node<T>* nextNode = curNode->nextNode;
+                delete curNode;
+                curNode = nextNode;
 
-    void push_back (const T& v) {
-        node<T>* newNode = new node<T>{v, nullptr, tail};
-        
-        if (tail != nullptr) {
-            tail->nextNode = newNode;
-        } else {
+            }
+
+            head = tail = nullptr;
+            size = 0;
+        }
+
+        void push_front (const T& v) {
+            node<T>* newNode = new node<T>{v, head, nullptr};
+
+            if (head != nullptr) head->prevNode = newNode;
+
             head = newNode;
-        }
-        
-        tail = newNode;
-
-        size++;
-    } 
-
-    void pop_front() {
-        if (head == nullptr) throw std::runtime_error("List is empty");
-
-        node<T>* temp = head;
-        head = head->nextNode;
-
-        if (head != nullptr) {
-            head->prevNode = nullptr;
-        } else {
-            tail = nullptr;
+            if (tail == nullptr) tail = newNode;
+            size++;
         }
 
-        delete temp;
-        size--;
-    }
+        void push_back (const T& v) {
+            node<T>* newNode = new node<T>{v, nullptr, tail};
+            
+            if (tail != nullptr) {
+                tail->nextNode = newNode;
+            } else {
+                head = newNode;
+            }
+            
+            tail = newNode;
 
-    void pop_back() {
-        if (tail == nullptr) throw std::runtime_error("List is empty");
+            size++;
+        } 
 
-        node<T>* temp = tail;
-        tail = tail->prevNode;
+        void pop_front() {
+            if (head == nullptr) throw std::runtime_error("List is empty");
 
-        if (tail != nullptr) {
-            tail->nextNode = nullptr;
-        } else {
-            head = nullptr;
+            node<T>* temp = head;
+            head = head->nextNode;
+
+            if (head != nullptr) {
+                head->prevNode = nullptr;
+            } else {
+                tail = nullptr;
+            }
+
+            delete temp;
+            size--;
         }
 
-        delete temp;
-        size--;
-    }
+        void pop_back() {
+            if (tail == nullptr) throw std::runtime_error("List is empty");
 
-    List& operator=(const List& rhs) {
-        if (this == &rhs) return *this;
-        clear();
+            node<T>* temp = tail;
+            tail = tail->prevNode;
 
-        for (node<T>* cur = rhs.head; cur != nullptr; cur = cur->nextNode) {
-            push_back(cur->value);
+            if (tail != nullptr) {
+                tail->nextNode = nullptr;
+            } else {
+                head = nullptr;
+            }
+
+            delete temp;
+            size--;
         }
 
-        return *this;
-    }
+        List& operator=(const List& rhs) {
+            if (this == &rhs) return *this;
+            clear();
 
-};
+            for (node<T>* cur = rhs.head; cur != nullptr; cur = cur->nextNode) {
+                push_back(cur->value);
+            }
+
+            return *this;
+        }
+
+    };
+}
