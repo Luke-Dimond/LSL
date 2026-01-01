da12082952443117cfcdee7d3d9af4750672a04d
Luke-Dimond <LukeManuelMG@gmail.com>
2026-01-01 12:15:26 -0500

    added readlinefromfile func

--- lsl/include/utility.hpp
+++ lsl/include/utility.hpp
@@ -11,9 +11,9 @@ namespace lsl {
 /*
 *  Converts a string into a numeric value.
 *
-*  @param s Input string to parse
-*  @param num Output variable that receives the parsed value
-*  @return true if the entire string is successfully parsed as a number; false otherwise (invalid argument, out of range, partial parse)
+*  @param s: Input string to parse
+*  @param num: Output variable that receives the parsed value
+*  @return bool: true if the entire string is successfully parsed as a number; false otherwise (invalid argument, out of range, partial parse)
 */
 template <typename T>
 bool stringToNum(const std::string& s, T& num) {
--- src/utility.cpp
+++ src/utility.cpp
@@ -1,9 +1,26 @@
 #include "utility.hpp"
+#include <vector>
+#include <fstream>
+#include <sstream>
 
-bool stringToNum(const std::string& s, int& num) {
-    std::from_chars(s.data(),s.data() + s.size(), num);
-}
+/*
+*  Empties a vector then reads a line from a file into it
+*
+*  @param file: File that will be read
+*  @param vec: vector values will be placed into 
+*  @return true if the function read a line; returns false if EOF
+*/
+bool readLineToVector(std::ifstream& file, std::vector<std::string>& vec) {
 
-bool stringToNum(const std::string& s, float& num) {
-    std::from_chars(s.data(),s.data() + s.size(), num);
+	std::string token{};
+	vec.clear();
+	if (!std::getline(file, token)) return false;
+
+	std::istringstream iss(token);
+
+	while (iss >> token) {
+		vec.push_back(token);
+	}
+
+	return true;
 }
