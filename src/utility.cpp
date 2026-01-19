1afc95ad5f9d1f4fae943e1de2c9db0403e4a1a5
Luke-Dimond <LukeManuelMG@gmail.com>
2026-01-19 18:51:25 -0500

    added utility.cpp to lsl namespace

--- src/utility.cpp
+++ src/utility.cpp
@@ -3,24 +3,27 @@
 #include <fstream>
 #include <sstream>
 
-/*
-*  Empties a vector then reads a line from a file into it
-*
-*  @param file: File that will be read
-*  @param vec: vector values will be placed into 
-*  @return true if the function read a line; returns false if EOF
-*/
-bool readLineToVector(std::ifstream& file, std::vector<std::string>& vec) {
+namespace lsl {
 
-	std::string token{};
-	vec.clear();
-	if (!std::getline(file, token)) return false;
+	/*
+	*  Empties a vector then reads a line from a file into it
+	*
+	*  @param file: File that will be read
+	*  @param vec: vector values will be placed into 
+	*  @return true if the function read a line; returns false if EOF
+	*/
+	bool readLineToVector(std::ifstream& file, std::vector<std::string>& vec) {
 
-	std::istringstream iss(token);
+		std::string token{};
+		vec.clear();
+		if (!std::getline(file, token)) return false;
 
-	while (iss >> token) {
-		vec.push_back(token);
+		std::istringstream iss(token);
+
+		while (iss >> token) {
+			vec.push_back(token);
+		}
+
+		return true;
 	}
-
-	return true;
 }
