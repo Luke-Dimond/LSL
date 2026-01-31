b2648823d5e4b84275606aeed9831a462768a866
Luke-Dimond <LukeManuelMG@gmail.com>
2026-01-30 20:48:17 -0500

    Made a static library

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,4 +1,4 @@
-#include "utility.hpp"
+#include <lsl/utility.hpp>
 #include <vector>
 #include <fstream>
 #include <sstream>
@@ -23,49 +23,6 @@ namespace lsl {
 		}
 
 		return true;
-	}
-
-	namespace Random {
-		static std::mt19937_64 rd{ std::random_device{}() };
-
-		/*
-		*  Generates a random integer between two numbers. [low, high]
-		*/
-		int randomNumber(int low, int high) {
-			std::uniform_int_distribution<int> dist(low, high);
-			return dist(rd);
-		}
-
-		/*
-		*  Generates a random float between two numbers. [low, high)
-		*/
-		float randomNumber(float low, float high) {
-			std::uniform_real_distribution<float> dist(low, high);
-			return dist(rd);
-		}
-
-		/*
-		*  Generates a random double between two numbers. [low, high)
-		*/
-		double randomNumber(double low, double high) {
-			std::uniform_real_distribution<double> dist(low, high);
-			return dist(rd);
-		}
-
-		/*
-		*  Sets the seed for the random number generator.
-		*/
-		void setSeed(uint64_t seed) {
-			rd.seed(seed);
-		}
-
-		/*
-		*  Sets the seed for the random number generator to a random number.
-		*
-		*/
-		void randomizeSeed() {
-			rd.seed(std::random_device{}());
-		}
 	}
 
 
